86f53f70d2301064ce2cc830340940b3fa90e1f6
MarkoW <albertnov@internet.ru>
2023-02-24 19:15:12 +0700

    Added a project which count a lucky tickets

---
+++ Lucky_Ticket/Lucky_Ticket.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+long lucky_tickets(long size) {
+	long count = 0;
+
+	long start = pow(10, size / 2);
+	long end = pow(10, size / 2) * 2;
+
+	for (long i = start; i < end; i++)
+	{
+		for (long j = start; j < end; j++)
+		{
+			if (i % 10 + (i % 100) / 10 + (i / 100) % 10 == j % 10 + (j % 100) / 10 + (j / 100) % 10) {
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
+
+
+int main()
+{
+	string str;
+
+	cout << "Enter tiraj: "; cin >> str;
+	int size = str.length() - 1;
+	cout << "Amount of lucky tickets: " << lucky_tickets(size) << endl;
+	cout << endl << "Veroyatnost': kajdiy " << pow(10, size) / lucky_tickets(size) << " bilet, bilet bydet s4astlivym" << endl;
+}
